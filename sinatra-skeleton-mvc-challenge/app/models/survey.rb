21dbfe9ed5739f54aa07cf10a0601e4d34a910ef
Christopher Mamian <christophermamian@gmail.com>
2014-11-06 12:01:04 -0800

    added models and migrations, associations

--- sinatra-skeleton-mvc-challenge/app/models/survey.rb
+++ sinatra-skeleton-mvc-challenge/app/models/survey.rb
@@ -1,3 +1,7 @@
 class Survey < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :user
+  has_many :questions
+  has_many :choices, through: :questions
+  has_many :answers, through: :choices
+
 end
--- sinatra-skeleton-mvc-challenge/db/migrate/20141106102938_create_choices.rb
+++ sinatra-skeleton-mvc-challenge/db/migrate/20141106102938_create_choices.rb
@@ -1,4 +1,10 @@
 class CreateChoices < ActiveRecord::Migration
   def change
+    create_table :choices do |t|
+      t.string :content
+      t.references :question
+
+      t.timestamps
+    end
   end
 end
