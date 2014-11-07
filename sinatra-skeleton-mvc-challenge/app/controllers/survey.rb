0f400b51ad74d5dad489d7a12aa72324168e6cc9
Christopher Mamian <christophermamian@gmail.com>
2014-11-07 14:37:50 -0800

    updating survey

--- sinatra-skeleton-mvc-challenge/app/controllers/survey.rb
+++ sinatra-skeleton-mvc-challenge/app/controllers/survey.rb
@@ -4,24 +4,20 @@
 end
 
 get '/surveys/new' do
+	erb :new_survey
 end
 
 post '/surveys/new' do
-	p params
 	survey = Survey.create(title: params[:title])
 	question = Question.create(question: params[:question], survey_id: survey.id)
 	choice = Choice.create(content: params[:content], question_id: question.id)
+
 end
 
-post '/questions/new' do
-end
-
-post '/choices/new' do
-end
-
-get '/question_form' do 
+get 'surveys/:survey_id/question_form/:question_id' do 
 	erb :_question, :layout => false
 end
+
 
 get '/choice_form' do
 	erb :_choice, :layout => false
--- sinatra-skeleton-mvc-challenge/public/js/application.js
+++ sinatra-skeleton-mvc-challenge/public/js/application.js
@@ -1,30 +1,49 @@
  
-$("body").on("click", "#add_question_button", function(e) {
-	e.preventDefault();
-	var ajaxRequest = $.ajax ({
-		url: '/question_form',
-		type: 'GET',
-	}).done(addFunction.bind(this))})
+// $("body").on("click", "#add_question_button", function(e) {
+// 	e.preventDefault();
 
-function addFunction (sentBackData) {
-		debugger;
-		var new_name = $('.container .container2').children("input").length
-		var updated_num_name = $(sentBackData).find('input[name="question[]"]').attr("name", "question[" +new_name + "]")
-		$('.question_partial_target').append(updated_num_name)	
-}
+// 	var ajaxRequest = $.ajax ({
+// 		url: '/question_form',
+// 		type: 'GET',
+// 	}).done(addFunction.bind(this))
+// })
 
-$('body').on("click", "#add_choice_button", function(e){
-	e.preventDefault();
+// function addFunction (sentBackData) {
+// 	$('.question_partial_target').append(sentBackData);
+// 	var question_counter = $('.question-container').length + 1;
+// 	var replace = $(sentBackData).find('input[name="question[]"]').attr("name", "question[" +new_name + "]")
+// 	console.log(replace)
+// 	// console.log(replace)
+// 	$("<input type='text' name='question[]' placeholder='Question'>").replaceWith(replace);
+
+// 	// console.log(sentBackData)
+// 	// console.log("counter: " + question_counter)
 	
-	var ajaxRequest = $.ajax({
-		url: '/choice_form',
-		type: 'GET'
-	}).done(namedFunction.bind(this))
-})
-	function namedFunction(sentBackData) {
-		var div = this.parentNode;
-		$(div).append(sentBackData)
-	}
+// 	// console.log(replace)
+
+// 	// $('.question_partial_target').append(sentBackData)
+// 	// console.log(sentBackData)
+// 	// var new_name = $('.question-container').length
+// 	// console.log(new_name)
+// 	// var new_name = $('.container .container2').children("input").length
+// 	// var updated_num_name = $(sentBackData).find('input[name="question[]"]').attr("name", "question[" +new_name + "]")
+// 	// console.log(updated_num_name)
+// 	// $('.question_partial_target').append(sentBackData)	
+		
+// }
+
+// $('body').on("click", "#add_choice_button", function(e){
+// 	e.preventDefault();
+	
+// 	var ajaxRequest = $.ajax({
+// 		url: '/choice_form',
+// 		type: 'GET'
+// 	}).done(namedFunction.bind(this))
+// })
+// 	function namedFunction(sentBackData) {
+// 		var div = this.parentNode;
+// 		$(div).append(sentBackData)
+// 	}
 
 
 $('#add_survey_button').on("click", someFunction);
@@ -42,11 +61,9 @@ function someFunction() {
 	$('#add_survey_button').off()
 }
 
-
-
-
-
-
+$(function(){
+	$("#draggable").draggable();
+})
 
 
 
